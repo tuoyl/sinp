75a2b1a7b146f6f7ac9705b0edd8be4c90730a2b
Youli <youlituo@gmail.com>
2025-06-24 13:58:53 +0200

    modify the absorption module

--- sinp/utils/refdata.py
+++ sinp/utils/refdata.py
@@ -1,20 +1,19 @@
-import pooch
+import importlib.resources as pkg_resources
 from astropy.io import fits
-import numpy as np
-
-# Configure the file manager
-table_manager = pooch.create(
-    path=pooch.os_cache("jaxspec"),
-    base_url="https://github.com/renecotyfanboy/jaxspec-database/raw/main/",
-    registry={
-        "xsect_tbabs_wilm.fits": "sha256:3cf45e45c9d671c4c4fc128314b7c3a68b30f096eede6b3eb08bf55224a44935",
-    }
-)
 
 # Load cross-section data into memory
 def load_tbabs_cross_section():
-    file_path = table_manager.fetch("xsect_tbabs_wilm.fits")
-    with fits.open(file_path) as hdul:
-        energy = hdul[1].data["ENERGY"]  # keV
-        sigma = hdul[1].data["SIGMA"]  # cm^2
+
+    with pkg_resources.files("sinp.utils.absorption").joinpath("xsect_tbabs_wilm.fits").open("rb") as f:
+        with fits.open(f) as hdulist:
+            energy = hdulist[1].data["ENERGY"]  # keV
+            sigma = hdulist[1].data["SIGMA"]  # cm^2
+    return energy, sigma
+
+def load_phabs_cross_section():
+
+    with pkg_resources.files("sinp.utils.absorption").joinpath("xsect_phabs_aspl.fits").open("rb") as f:
+        with fits.open(f) as hdulist:
+            energy = hdulist[1].data["ENERGY"]  # keV
+            sigma = hdulist[1].data["SIGMA"]  # cm^2
     return energy, sigma
